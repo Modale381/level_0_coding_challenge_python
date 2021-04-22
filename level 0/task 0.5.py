2db926dc4a7f2b96ecf1f17a2c78cf62660b7a18
Modale381 <76646019+Modale381@users.noreply.github.com>
2021-04-22 20:30:03 +0200

    Delete level 0 directory

--- level 0/task 0.5.py
+++
@@ -1,5 +0,0 @@
-def triangle(d,e,f):
-    g = (d + e+ f)/2
-    return ((g *(g-d)*(g-e)*(g-f)** 0.5))
-
-print(triangle(14,16,18))
--- level 0/task 0.8.py
+++
@@ -1,6 +0,0 @@
-def time_conv(num):
-    hours = num//60
-    minutes = num%60
-    return (hours,minutes)
-
-print(time_conv(84))
